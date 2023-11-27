808d821e964399d058ea24e3c53d615f2b71f14c
Dakar <namnam982002@gmail.com>
2023-11-27 21:42:26 +0700

    final commit

--- eTickets/Models/ShoppingCartItem.cs
+++ eTickets/Models/ShoppingCartItem.cs
@@ -10,11 +10,8 @@ namespace webTech.Models
     {
         [Key]
         public int Id { get; set; }
-
         public Movie Movie { get; set; }
         public int Amount { get; set; }
-
-
         public string ShoppingCartId { get; set; }
     }
 }
